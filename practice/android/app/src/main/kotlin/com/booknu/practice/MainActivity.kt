e327943e72f6aa48bd115d283b1bbec3b551f1f2
booknu <lim551@naver.com>
2023-04-09 22:26:39 +0900

    feature: move project directory

--- practice/android/app/src/main/kotlin/com/booknu/practice/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.booknu.practice
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ practice_proj/android/app/src/main/kotlin/com/booknu/practice_proj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.booknu.practice_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
